85a3239536fc12a88f2df7f20753004ce2799ec3
xzwbsz <44642002+xzwbsz@users.noreply.github.com>
2023-02-28 21:38:29 +0800

    Add files via upload

---
+++ code/add_120/add_120_ecmf_olr.py
@@ -0,0 +1,126 @@
+#导入相关包
+from matplotlib.pyplot import axis
+import numpy as np
+import os
+#import matplotlib.pyplot as plt
+import random
+import netCDF4
+import datetime
+import seaborn as sns
+from global_land_mask import globe
+from scipy import interpolate
+#plt.rcParams['font.sans-serif'] = ['SimHei'] #中文支持
+#%matplotlib inline
+#再分析数据olr
+re_data=netCDF4.Dataset('/WdHeDisk/users/zhangnong/MJO/new_DATA/olr_1950-2022.nc')
+re_data_olr=np.array(re_data.variables['olr'])
+re_data_time=np.array(re_data.variables['time'])
+#获取文件名
+def get_files():
+    for filepath,dirnames,filenames in os.walk(r'/WdHeDisk/users/zhangnong/MJO/711_test/s2s_data/ecmf/OLR/'):
+        for filename in filenames:
+            print (filename)
+    return filenames
+
+filenames=get_files()
+#读取数据,写个函数
+def read_data(filename):
+    path='/WdHeDisk/users/zhangnong/MJO/711_test/s2s_data/ecmf/OLR/'
+    new_path='/WdHeDisk/users/zhangnong/MJO/711_test/add_120_re_data/ecmf/OLR/'
+    data=netCDF4.Dataset(path+filename)
+    print("date:",filename)
+    #分别提取数据
+    lat=np.array(data.variables['latitude'])
+    lon=np.array(data.variables['longitude'])
+    #插入前120天
+    time=np.array(data.variables['time'])
+    data_olr=np.array(data.variables['olr'][:,:,:])
+    #定位到再分析数据位置
+    location=np.argwhere(re_data_time==time[0])
+    print("location:",int(location))
+    #考虑闰年
+    run_year=np.array([200403,200404,200405,200406,200803,200804,200805,200806,201203,201204,201205,201206,201603,201604,201605,201606,
+                      202003,202004,202005,202006])
+    run_year_2=np.array([200402,200802,201202,201602,202002])
+    run_year_3=np.array([20040121,20080121,20120121,20160121,20200121])
+    defind_run=int(filename[0:6])
+    true_date=int(filename[0:8])
+    #如果受闰年影响，我们需要加121天才能对齐
+    if defind_run in run_year:
+        fina_location=int(location)
+        begin_location=fina_location-121
+        #输出一下开始时间
+        #start_end_time(re_data,begin_location)
+        insert_data=re_data_olr[begin_location:fina_location,:,:]
+        print("insert data shape:",insert_data.shape)
+        print("olr data shape:",data_olr.shape)
+        fina_data_ttr=np.concatenate((insert_data,data_olr),axis=0)
+        print("fina data shape:",fina_data_ttr.shape)
+    elif (defind_run in run_year_2)|(true_date in run_year_3):
+        trans_time=time[45]+24
+        mou_location=np.argwhere(re_data_time==trans_time)
+        mou_location_int=int(mou_location)
+        in_data1=re_data_olr[mou_location_int,:,:]
+        insert_data1=np.squeeze(in_data1)
+        print("in_data1 shape",in_data1.shape)
+        print("insert_data1 shape",insert_data1.shape)
+        insert_data1=np.expand_dims(insert_data1,0)
+        data_olr1=np.concatenate((data_olr,insert_data1),axis=0)
+        #添加前120天
+        fina_location=int(location)
+        begin_location=fina_location-120
+        #输出一下开始时间
+        #start_end_time(re_data,begin_location)
+        insert_data=re_data_olr[begin_location:fina_location,:,:]
+        print("insert data shape:",insert_data.shape)
+        print("olr data shape:",data_olr1.shape)
+        fina_data_ttr=np.concatenate((insert_data,data_olr1),axis=0)
+        print("fina data shape:",fina_data_ttr.shape)  
+        time=np.append(time,[trans_time],axis=0) 
+
+    else:
+        #添加前120天
+        fina_location=int(location)
+        begin_location=fina_location-120
+        #输出一下开始时间
+        #start_end_time(re_data,begin_location)
+        insert_data=re_data_olr[begin_location:fina_location,:,:]
+        print("insert data shape:",insert_data.shape)
+        print("olr data shape:",data_olr.shape)
+        fina_data_ttr=np.concatenate((insert_data,data_olr),axis=0)
+        print("fina data shape:",fina_data_ttr.shape)
+    length=len(fina_data_ttr)
+    print("length:",length)
+    #构造新的时间函数，需要拼接，要不然越界
+    first_time=np.array(re_data_time[begin_location:fina_location])
+    new_time=np.concatenate((first_time,time),axis=0)
+    #构造新的nc文件
+    da_olr=netCDF4.Dataset(new_path+filename[0:8]+'_add_120redata_ecmf_olr.nc','w',format='NETCDF4')
+    da_olr.createDimension('latitude', 13)  # 创建坐标点
+    da_olr.createDimension('longitude', 144)
+    da_olr.createDimension('time',length)
+    #设置变量
+    lat_var = da_olr.createVariable("latitude",'f4',('latitude') )  #添加coordinates  'f'为数据类型，不可或缺
+    lat_var.units = 'degrees_north'
+    lon_var = da_olr.createVariable("longitude",'f4',('longitude'))  #添加coordinates  'f'为数据类型，不可或缺
+    lon_var.units = 'degrees_east'
+    time_var=da_olr.createVariable("time",'i4',('time'))
+    time_var.units= 'hours since 1900-01-01 00:00:00.0'
+    #填充数据
+    da_olr.variables['latitude'][:]=lat[:]   #填充数据
+    da_olr.variables['longitude'][:]=lon[:]   #填充数据
+    da_olr.variables['time'][:]=new_time[:]
+    #设置u200变量
+    olr =da_olr.createVariable('olr','f',('time','latitude','longitude'))
+    olr.units = 'W m**-2'
+    #填充数据
+    da_olr.variables['olr'][:,:,:]=fina_data_ttr[:,:,:]
+    da_olr.close()
+    print("success to create olr!!!")
+    return 
+
+for i in range (len(filenames)):
+    read_data(filename=filenames[i])
+
+print("trans all success!!!")
+
---
+++ code/cal_seasonal_cycle/seasonal_babj_olr.py
@@ -0,0 +1,117 @@
+#导入相关包
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import random
+import netCDF4
+import datetime
+import seaborn as sns
+from global_land_mask import globe
+from scipy import interpolate
+#plt.rcParams['font.sans-serif'] = ['SimHei'] #中文支持
+#%matplotlib inline
+#定位每个2.29号的time
+#输入参数datetime是包含年月日的数字，例如20080311
+def find_229_location(date_time):
+    #取年份
+    date_year=date_time//10000
+    start_time=datetime.datetime(1900,1,1,0,0)
+    end_time=datetime.datetime(date_year,2,29,0,0)
+    date=end_time-start_time
+    date_hour=date.days*24
+    #print(type(date_hour))
+    #print(date_hour)
+    return date_hour
+#获取文件名
+filenames=np.loadtxt('/WdHeDisk/users/zhangnong/MJO/711_test/2006_babj_date_sort.txt')
+print('------')
+print(filenames[0])
+print(len(filenames))
+#封装函数
+def cal_seasonal_cycle(filename):
+    path='/WdHeDisk/users/zhangnong/MJO/711_test/add_120_re_data/babj/OLR/'
+    #提取文件名的关键字
+    date_time=int(filename)
+    print(filename)
+    date_time_fina=date_time+160000
+    #print(date_time)
+    #print(date_time_fina)
+    date_yearmonth=date_time//100
+    date_month=date_yearmonth%100
+    #date_year=date_time//10000
+    run_year=np.array([2008,2012,2016,2020])
+    #1～6月需要考虑2.29情况
+    if date_month < 7:
+        data_16year_1=[]#不包含2.29的版本
+        #闰年的几个数组，放在一起，一个有四个，这四个做平均
+        data_run_229 = []
+        for i in range(date_time,date_time_fina,10000):
+            new_filename=str(i)
+            date_year=i//10000
+            data=netCDF4.Dataset(path+new_filename+'_add_120redata_babj_olr.nc')
+            data_olr=np.array(data.variables['olr'])
+            data_time=np.array(data.variables['time'])
+            print("mean data:",data_olr.shape)
+            #删除1的维度，应该变（180，13，144）
+            if date_year in run_year:
+                #闰年多了一天，（181，13，144），第一个版本，就是把2.29位置找到，然后删除他，再融合，适合不是闰年的
+                #首先找到对应年份的2.29号
+                date_hour=find_229_location(i)
+                print("date_hour:",date_hour)
+                #找到其对应的位置
+                location=int(np.argwhere(data_time==int(date_hour)))
+                print("location:",location)
+                #把2.29的内容压入数组，并在原数组删除
+                data_run_229.append(data_olr[location,:,:])
+                print(data_olr[location,9,7])
+                data_olr=np.delete(data_olr,location,axis=0)
+                print("run_year henxin shape:",data_olr.shape)
+            data_16year_1.append(data_olr)
+        data_16year_1=np.array(data_16year_1)
+        data_run_229=np.array(data_run_229)
+        print(data_run_229[:,9,7])
+        print("first banben:",data_16year_1.shape)
+        print("run data shape:",data_run_229.shape)
+        #平均16年
+        mean_data_1=np.mean(data_16year_1,axis=0)
+        mean_data_run=np.mean(data_run_229,axis=0)
+        print(mean_data_run[9,7])
+        print("mean data1 shape:",mean_data_1.shape)
+        print("mean run data shape:",mean_data_run.shape)
+        #构造一个适合181天的版本
+        mean_data_2=np.insert(mean_data_1,location,mean_data_run,axis=0)
+        print("mean data2 shape:",mean_data_2.shape)
+        print(mean_data_2[176,9,7])
+        #写入文件，前6个月有两个版本，方便后面rmm计算的时候减
+        save_filename=str(filename)
+        save_filename=save_filename[4:8]
+        np.save('/WdHeDisk/users/zhangnong/MJO/711_test/seasonal_cycle/babj/OLR/'+save_filename+'_seasonal_cycle_olr.npy', mean_data_1)
+        print("save 1 success!!!")
+        np.save('/WdHeDisk/users/zhangnong/MJO/711_test/seasonal_cycle/babj/OLR/'+save_filename+'_run_seasonal_cycle_olr.npy', mean_data_2)
+        print("save 2 success!!!")
+    else:
+        #符合babj的7，8，9，10，11，12月情况，不包含二月
+        data_16year=[]
+        for i in range(date_time,date_time_fina,10000):
+            new_filename=str(i)
+            data=netCDF4.Dataset(path+new_filename+'_add_120redata_babj_olr.nc')
+            data_olr=np.array(data.variables['olr'])
+            #多个样本，平均一下
+            print("mean data:",data_olr.shape)
+            #删除1的维度，应该变（180，13，144）
+            data_16year.append(data_olr)
+        data_16year=np.array(data_16year)
+        print("get 16years data:",data_16year.shape)
+        #平均16年
+        mean_data=np.mean(data_16year,axis=0)
+        print("mean data shape:",mean_data.shape)
+        save_filename=str(filename)
+        save_filename=save_filename[4:8]
+        np.save('/WdHeDisk/users/zhangnong/MJO/711_test/seasonal_cycle/babj/OLR/'+save_filename+'_seasonal_cycle_olr.npy', mean_data)
+        print("save data success!!!")
+    return
+for i in range (len(filenames)):
+    str_filename=str(int(filenames[i]))
+    cal_seasonal_cycle(filename=filenames[i])
+
+print("save all success!!!")
